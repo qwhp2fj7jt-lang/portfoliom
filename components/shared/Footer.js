ec8e4dcb50fbba3c08c5e4606f3388f26d18f7dc
Zeynep <101248567+zeynepbass@users.noreply.github.com>
2025-12-13 23:37:36 +0300

    .

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     const t = useTranslations("Footer");
     return (
       
-      <footer className="w-full h-[20vh] border-t text-gray-400 dark:border-gray-800  border-gray-200 flex items-center pl-[15%] ">
+      <footer className="w-full h-[20vh] border-t text-gray-700 dark:text-gray-300 dark:border-gray-800  border-gray-200 flex items-center pl-[5%] ">
         <div className="container mx-auto px-6 md:px-10 py-10">
           <p>{t("footerParagraf1")}</p>
           <p className="flex items-center gap-2">
