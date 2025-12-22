ad6bc8a1a36471b378936aac9dd20bfac263dee1
Zeynep <101248567+zeynepbass@users.noreply.github.com>
2025-12-22 21:04:43 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@ const nextConfig = {
     async redirects() {
       return [
         {
-          source: '/',
-          destination: '/tr',
-          permanent: true,
-        },
-        {
           source: '/about',
           destination: '/tr/about',
           permanent: true,
