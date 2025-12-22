3e4fc4bc5a56a77553005a1eb100940520b237ec
Zeynep <101248567+zeynepbass@users.noreply.github.com>
2025-12-22 21:26:24 +0300

    .

---
+++ app/[locale]/about/layouts.js
@@ -0,0 +1,18 @@
+export const metadata = {
+    title: "Hakkımda | Zeynep Baş",
+    description:
+      "Zeynep Baş kimdir? React ve Next.js ile modern web uygulamaları geliştiren frontend geliştirici.",
+  
+    alternates: {
+      canonical: "/tr/about",
+      languages: {
+        tr: "/tr/about",
+        en: "/en/about",
+      }, 
+    },
+  };
+  
+  export default function Layout({ children }) {
+    return children;
+  }
+  
--- middleware.js
+++ middleware.js
@@ -1,1 +1,10 @@
-export function middleware() {}
+import createMiddleware from "next-intl/middleware";
+
+export default createMiddleware({
+  locales: ["en", "tr"],
+  defaultLocale: "tr",
+});
+
+export const config = {
+  matcher: ["/", "/(tr|en)/:path*"],
+};
