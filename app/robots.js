49240b49761e051a61c63e459894635334f3843b
Zeynep <101248567+zeynepbass@users.noreply.github.com>
2025-12-22 22:21:55 +0300

    .

---
+++ app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+    return {
+      rules: {
+        userAgent: "*",
+        allow: "/",
+      },
+      sitemap: "https://zeynepbas.dev/sitemap.xml",
+    };
+  }
+  
---
+++ app/sitemap.js
@@ -0,0 +1,21 @@
+export default function sitemap() {
+    return [
+      {
+        url: "https://zeynepbas.dev/tr",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://zeynepbas.dev/en",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://zeynepbas.dev/tr/about",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://zeynepbas.dev/en/about",
+        lastModified: new Date(),
+      },
+    ];
+  }
+  
