a6eeb43da7e98e65dc6ab81e194b6ca66a4cc475
Zeynep <101248567+zeynepbass@users.noreply.github.com>
2025-12-22 21:03:17 +0300

    .

--- middleware.js
+++ middleware.js
@@ -1,10 +1,1 @@
-import createMiddleware from "next-intl/middleware";
-
-export default createMiddleware({
-  locales: ["en", "tr"],
-  defaultLocale: "en",
-});
-
-export const config = {
-  matcher: ["/", "/(tr|en)/:path*"],
-};
+export function middleware() {}
