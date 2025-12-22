15be2eccc7941ad0b5e89314168c5501aa37a98f
Zeynep <101248567+zeynepbass@users.noreply.github.com>
2025-12-22 22:17:34 +0300

    .

--- middleware.js
+++ middleware.js
@@ -3,8 +3,11 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
   locales: ["en", "tr"],
   defaultLocale: "tr",
+  localePrefix: "always",
 });
 
 export const config = {
-  matcher: ["/", "/(tr|en)/:path*"],
+  matcher: [
+    "/((?!_next|favicon.ico|icon.png|images|robots.txt|sitemap.xml|google9bb6d035c1ce6bb6.html).*)",
+  ],
 };
