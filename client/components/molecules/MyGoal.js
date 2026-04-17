1fd35fbc87c2291a67c75afaa120d0a7056bd568
zeynep bas <qwhp2fj7jt@privaterelay.appleid.com>
2026-04-17 22:49:22 +0300

    s

--- client/components/molecules/MyGoal.js
+++ client/components/molecules/MyGoal.js
@@ -1,9 +1,9 @@
 export default function MyGoal({ baslik, paragraf, baglanti }) {
   return (
 
-      <div className="mt-3">
+      <div className="m-0">
           <div className="flex justify-between">
-        <h2 className="text-xl font-bold mb-3 dark:text-white">{baslik}</h2>
+        <h2 className="text-xl font-bold mb-1 dark:text-white">{baslik}</h2>
         {baglanti && baglanti}
         </div>
         <p className="text-gray-600 dark:text-white">{paragraf}</p>
