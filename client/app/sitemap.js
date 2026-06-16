3ab1f1ddbf077c7db881677fb6a9057fc6c60d89
zeynep bas <qwhp2fj7jt@privaterelay.appleid.com>
2026-06-16 15:55:23 +0300

    -

--- client/app/sitemap.js
+++ client/app/sitemap.js
@@ -1,11 +1,42 @@
-export default function sitemap() {
+export default async function sitemap() {
   const baseUrl = "https://portfoliom-4p84.onrender.com";
 
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/articles`,
+    { cache: "no-store" }
+  );
+
+  const articles = await res.json();
+
+  const blogUrls =
+    articles?.map((article) => ({
+      url: `${baseUrl}/blog/${article.article.slug}`,
+      lastModified: new Date(article.article.updatedAt || article.article.createdAt),
+    })) || [];
+
   return [
-    { url: `${baseUrl}`, lastModified: new Date() },
-    { url: `${baseUrl}/about`, lastModified: new Date() },
-    { url: `${baseUrl}/projects`, lastModified: new Date() },
-    { url: `${baseUrl}/blog`, lastModified: new Date() },
-    { url: `${baseUrl}/zone`, lastModified: new Date() },
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/zone`,
+      lastModified: new Date(),
+    },
+
+
+    ...blogUrls,
   ];
 }
