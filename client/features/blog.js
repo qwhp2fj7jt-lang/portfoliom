0c38a66b42e9562c2af2cd33ecbd3d55297bdd27
zeynep bas <qwhp2fj7jt@privaterelay.appleid.com>
2026-06-16 19:56:28 +0300

    d

--- client/features/blog.js
+++ client/features/blog.js
@@ -1,7 +1,7 @@
 import { Papers } from "@/widgets";
 export default function Blog({ articles }) {
   return (
-    <div className="mx-auto max-w-6xl px-4 py-10">
+    <div className="mx-auto max-w-6xl px-4 py-10 h-[100vh]">
       <Papers articles={articles} />
     </div>
   );
