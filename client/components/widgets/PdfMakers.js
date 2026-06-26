06bacac00c55120c78f2463c84b5b0f9a6b61638
qwhp2fj7jt-lang <qwhp2fj7jt@privaterelay.appleid.com>
2026-06-27 00:32:09 +0300

    Update PdfMakers.js

--- client/components/widgets/PdfMakers.js
+++ client/components/widgets/PdfMakers.js
@@ -10,7 +10,7 @@ export default function PdfMakers({
           <div className="bg-white w-[90%] h-[90%] rounded-xl relative overflow-hidden">
             <button
               onClick={() => setOpenPdf(false)}
-              className="absolute top-2 right-2 bg-black text-white px-2 py-1 rounded z-10"
+              className="absolute top-2 left-2 bg-black text-white px-2 py-1 rounded z-10"
             >
               ✕
             </button>
