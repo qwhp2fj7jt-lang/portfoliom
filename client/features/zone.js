180c46a99fbfbbe82c9a946470907b2f36135fc6
qwhp2fj7jt-lang <qwhp2fj7jt@privaterelay.appleid.com>
2026-06-28 23:28:41 +0300

    Update zone.js

--- client/features/zone.js
+++ client/features/zone.js
@@ -3,7 +3,7 @@
 import { CardZone } from "@/components/widgets";
 export default function Zone({ product }) {
   return (
-    <div className="mx-auto max-w-6xl px-4 py-10">
+    <div className="mx-auto max-w-4xl px-4 py-10">
       <CardZone posts={product} />
     </div>
   );
