5e4aefdef9591556a81c018e603ed4947ea64ded
qwhp2fj7jt-lang <qwhp2fj7jt@privaterelay.appleid.com>
2026-06-28 23:39:32 +0300

    Update Footer.js

--- client/components/layouts/Footer.js
+++ client/components/layouts/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="relative mt-10 border-t text-center border-gray-200/50 dark:border-zinc-700/50 dark:bg-zinc-900/80 backdrop-blur">
       <div className="absolute top-0 left-0 w-full h-px bg-gradient-to-r from-transparent via-gray-300 dark:via-zinc-700 to-transparent" />
 
-      <div className="mx-auto max-w-6xl px-6 lg:px-12 py-12 flex flex-col md:flex-row items-start  gap-8">
+      <div className="mx-auto max-w-6xl px-6 lg:px-12 py-12 flex flex-col md:flex-row justify-between  gap-8">
         
  
         <div className="flex flex-col gap-2 text-sm text-gray-500 dark:text-gray-300  items-start">
