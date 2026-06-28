f6d9ed163ff05d7033f934e666ed4614c962d738
qwhp2fj7jt-lang <qwhp2fj7jt@privaterelay.appleid.com>
2026-06-28 23:28:33 +0300

    Update section.js

--- client/features/section.js
+++ client/features/section.js
@@ -2,7 +2,7 @@ import { Hero, ProjectsAndExperience, Blog } from "@/widgets";
 import { Skills } from "@/components/molecules";
 export default function HomeRoute({ articles }) {
   return (
-    <div className="mx-auto max-w-6xl px-4 py-10">
+    <div className="mx-auto max-w-4xl px-4 py-10">
       <Hero />
       <Skills />
       <Blog articles={articles} />
