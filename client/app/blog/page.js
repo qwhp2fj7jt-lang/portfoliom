b835cab3cfceb2cdb5fe3e230092321edd4aad94
qwhp2fj7jt-lang <qwhp2fj7jt@privaterelay.appleid.com>
2026-07-19 16:55:46 +0300

    Update page.js

--- client/app/blog/page.js
+++ client/app/blog/page.js
@@ -1,7 +1,7 @@
 import { postArticles } from "@/shared/services/postArticles";
 import SubBlog from "@/features/blog";
 export async function generateMetadata() {
-  const baseUrl = "https://portfoliom-4p84.onrender.com";
+  const baseUrl = "https://www.zeynepbas.dev";
 
   return {
     metadataBase: new URL(baseUrl),
