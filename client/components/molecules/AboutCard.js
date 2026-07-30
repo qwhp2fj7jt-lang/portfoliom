21d79ec0e2fc5f2decda9f888c83aa8feaad785a
zeynep bas <qwhp2fj7jt@privaterelay.appleid.com>
2026-07-30 17:04:04 +0300

    x

--- client/components/molecules/AboutCard.js
+++ client/components/molecules/AboutCard.js
@@ -4,7 +4,7 @@ export default function AboutCard({baslik,paragraf,paragrafOne,paragrafTwo}) {
 
   return (
     <>
- <h2 className="text-xl dark:text-gray-300 md:text-2xl text-gray-700 mb-2 text-center">
+ <h2 className="text-xl dark:text-gray-300 md:text-2xl text-gray-700 mb-2 text-left">
       {baslik}
       </h2>
       <p className="text-gray-700 dark:text-gray-400 text-sm md:text-base mb-2 leading-relaxed">
